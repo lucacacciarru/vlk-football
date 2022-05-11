1174affcc3f006902c88a5015e2aeca8e4fcbc3d
Luca Cacciarru <86778250+lucacacciarru@users.noreply.github.com>
2022-05-11 12:15:53 +0200

    Removed excess dependenciess

--- src/_shared/store/configureStore.ts
+++ src/_shared/store/configureStore.ts
@@ -1,8 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { reducer } from "./reducer";
-import { Saga } from "redux-saga";
 import createSagaMiddleware from "@redux-saga/core";
-import { createPlayerSaga } from "../../player/store/saga/create";
 import { createRootSaga } from "./sagas";
 
 const sagaMiddleware = createSagaMiddleware();
