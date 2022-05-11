70a45595db2d8bb38e9abc387640a9b734831eea
Luca Cacciarru <86778250+lucacacciarru@users.noreply.github.com>
2022-05-11 17:58:25 +0200

    Revert "Added the Icon component to render custom icons"

--- src/_shared/components/Icon/IconLibrary.ts
+++
@@ -1,9 +0,0 @@
-import { ReactComponent as hand } from "./assets/hand.svg";
-import { ReactComponent as ball } from "./assets/ball-football.svg";
-
-export const iconLibrary = {
-  hand,
-  ball,
-};
-
-export type IconLibrary = typeof iconLibrary;
