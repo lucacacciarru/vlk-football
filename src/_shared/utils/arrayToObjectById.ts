22c613d1a0f7cb28a77b43b77a1569ca11e15bc4
Luca Cacciarru <86778250+lucacacciarru@users.noreply.github.com>
2022-05-13 12:42:52 +0200

    Replaced redux-saga with RTK Query closes #31

--- src/_shared/utils/arrayToObjectById.ts
+++
@@ -1,9 +0,0 @@
-export function arrayToObjectById<T extends { id: string }>(array: T[]) {
-  return array.reduce(
-    (acc, item) => ({
-      ...acc,
-      [item.id]: item,
-    }),
-    {}
-  );
-}
---
+++ src/player/store/types.ts
@@ -0,0 +1,8 @@
+export type Player = {
+  id: string;
+  name: string;
+  rating: number;
+  goalkeeper: boolean;
+  sports?: string[];
+  avatar?: string;
+};
