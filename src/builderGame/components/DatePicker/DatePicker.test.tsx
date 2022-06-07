80e00e07cb10e3f793ece9b0f2bd57ef813907bc
Luca Cacciarru <86778250+lucacacciarru@users.noreply.github.com>
2022-06-07 11:08:22 +0200

    Added renderHook to DatePicker test

--- src/builderGame/components/DatePicker/DatePicker.test.tsx
+++ src/builderGame/components/DatePicker/DatePicker.test.tsx
@@ -1,11 +1,16 @@
 import { useForm } from 'react-hook-form';
 import { render } from '../../../_shared/testConfig/customRender';
+import { renderHook } from '../../../_shared/testConfig/customRenderHook';
 import { PlaceAndDate } from '../SelectPlaceAndDateForm';
 import { DatePicker } from './DatePicker';
 
 describe('DatePicker component', () => {
   test('Should be rendered', () => {
-    const { control } = useForm<PlaceAndDate>();
-    render(<DatePicker control={control} />);
+    const { result } = renderHook(() =>
+      useForm<PlaceAndDate>({
+        defaultValues: { date: new Date(), place: 'anyString' },
+      }),
+    );
+    render(<DatePicker control={result.current.control} />);
   });
 });
