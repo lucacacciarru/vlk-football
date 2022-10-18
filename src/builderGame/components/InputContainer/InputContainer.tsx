1eb2e517179b7032c8d2f118f6a83c839d26762e
Luca Cacciarru <luca.cacciarru@protonmail.com>
2022-10-18 14:11:45 +0200

    Deleted old logic to render the FormErrorMessage component

--- src/builderGame/components/InputContainer/InputContainer.tsx
+++ src/builderGame/components/InputContainer/InputContainer.tsx
@@ -4,12 +4,12 @@ import {
   FormErrorMessage,
   FormLabel,
 } from '@chakra-ui/react';
-import { PropsWithChildren, useMemo } from 'react';
+import { PropsWithChildren } from 'react';
 import { FieldError } from 'react-hook-form';
 
 type Props = {
   label: string;
-  error?: FieldError | FieldError[];
+  error?: FieldError;
 } & FormControlProps;
 
 export const InputContainer: React.FC<PropsWithChildren<Props>> = ({
@@ -18,16 +18,6 @@ export const InputContainer: React.FC<PropsWithChildren<Props>> = ({
   children,
   ...formControlProps
 }) => {
-  const renderErrors = useMemo(() => {
-    if (Array.isArray(error)) {
-      return error.map(singleError => (
-        <FormErrorMessage>{singleError?.message}</FormErrorMessage>
-      ));
-    }
-
-    return <FormErrorMessage>{error?.message}</FormErrorMessage>;
-  }, [error]);
-
   return (
     <FormControl isInvalid={!!error} w="full" {...formControlProps}>
       <FormLabel>{label}</FormLabel>
